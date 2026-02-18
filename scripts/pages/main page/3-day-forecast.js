4f9bea731974faab27d1b410da4fb521f790d0ca
Goodie <godlucmbise@gmail.com>
2026-02-18 12:33:34 +0300

    Refactor forecast display functions to display the data correctly

--- scripts/pages/main page/3-day-forecast.js
+++ scripts/pages/main page/3-day-forecast.js
@@ -1,16 +1,18 @@
 import dayjs from 'https://cdn.skypack.dev/dayjs';
 
+
 export function displayTenDayForecast(weatherData) {
     const weatherForecast = weatherData.forecast.forecastday;
-    weatherForecast.map(day => {
-        document.querySelector('.js-ten-day-forecast')
-            .innerHTML += `
+   const result = weatherForecast.map(day => 
+        `
             <section class="day-forecast">
                 <div>${dayjs(day.date).format('ddd')}</div>
                 <div>Hi: ${Math.round(day.day.maxtemp_c)} </div>
                 <img src="${day.day.condition.icon}">
                 <div>Lo: ${Math.round(day.day.mintemp_c)}</div>
             </section>
-            `;
-    }).join('');
+            `
+    ).join('');
+
+    return result;
 }
--- scripts/pages/main page/hourly-forecast.js
+++ scripts/pages/main page/hourly-forecast.js
@@ -3,14 +3,15 @@ import dayjs from 'https://cdn.skypack.dev/dayjs';
 export function displayHourlyForecast(weatherData) {
     const weatherForecast = weatherData.forecast.forecastday;
     const [firstDay] = weatherForecast;
-    return firstDay.hour.forEach(hourOfTheDay => {
-        document.querySelector('.js-hourly-forecast')
-            .innerHTML += `
+    const result = firstDay.hour.map(hourOfTheDay =>
+        `
             <div class="hour-forecast">
                 <div>${dayjs(hourOfTheDay.time).format('hh a')}</div>
                 <div>${Math.round(hourOfTheDay.temp_c)}\u00B0 C</div>
                 <div class="condition-in-hour-of-the-day"> ${hourOfTheDay.condition.text}</div>
             </div>
-            `;
-    }).join('');       
+        `
+    ).join('');
+    return result;
+    
 }
